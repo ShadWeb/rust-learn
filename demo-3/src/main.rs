45aa2723eda1d5ab672694c5543e58afe99baf6e
ShadWeb <shyifou@gmail.com>
2025-12-01 23:36:48 +0330

    start Variable boolean & chareter & integer

--- demo-3/src/main.rs
+++ demo-3/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    // boolean true false
+    let varboll: bool = true;
+    println!("boolean: {}", varboll);
+    //chareter
+    let varchar: char = 'a';
+    println!("char: {}", varchar);
+    //integer
+    let varint8: i8 = 127;
+    println!("integer i8: {}", varint8);
 }
