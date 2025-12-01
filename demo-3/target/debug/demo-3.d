ec18e466a4d9e6b38e48f21e1a3b0716d1f76b51
ShadWeb <shyifou@gmail.com>
2025-12-01 22:59:05 +0330

    start demo 3

---
+++ demo-3/target/debug/demo-3.d
@@ -0,0 +1,1 @@
+C:\Users\shyi8\OneDrive\Desktop\rust-learn\demo-3\target\debug\demo-3.exe: C:\Users\shyi8\OneDrive\Desktop\rust-learn\demo-3\src\main.rs
---
+++ demo-3/target/debug/deps/demo_3-903bbbcecb00fe03.d
@@ -0,0 +1,5 @@
+C:\Users\shyi8\OneDrive\Desktop\rust-learn\demo-3\target\debug\deps\demo_3-903bbbcecb00fe03.d: src\main.rs
+
+C:\Users\shyi8\OneDrive\Desktop\rust-learn\demo-3\target\debug\deps\demo_3-903bbbcecb00fe03.exe: src\main.rs
+
+src\main.rs:
